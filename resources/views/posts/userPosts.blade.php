404a80d4011873cb08d6692eec6a9a76b23744b8
obadasaeed <Opadasaeed@yahoo.com>
2019-03-30 15:52:14 +0200

    create user post view and create post view

---
+++ resources/views/posts/userPosts.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+@section('content')
+
+            <div class="content">
+                <div class="container">
+                <h1> Posts</h1>
+
+
+ <div class="row container">
+
+  <div class="col-sm-4">
+  
+  <div class="panel panel-primary">
+  <div class="panel-heading">
+    <h3 class="panel-title">title</h3>
+  </div>
+  <div class="panel-body">
+    <p>body</p>
+
+
+ <img src="https://loremflickr.com/320/240" class="img-thumbnail"  style="width:50%;height:50%" >
+
+   <span class="label label-danger">created at : ####  </span>
+     <span class="label label-info">  by :opada</span>
+
+      <a href="#" class="float-right">View Post</a>
+
+  </div>
+</div>
+  
+  </div>
+   
+
+
+</div>
+
+</div>
+ 
+</div>
