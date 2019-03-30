261689193afdb14c8413ba24629d2f6385b79e52
nNoha <noha.muhammed42@gmail.com>
2019-03-30 15:34:49 +0200

    add index view for posts and edit post view and view post view

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+@section('content')
+
+            <div class="content">
+                <div class="container">
+                <h1>Posts</h1>
+
+
+ <div class="row container">
+
+  <div class="col-sm-4">
+
+  <div class="panel panel-primary" style="overflow: hidden;">
+  <div class="panel-heading">
+    <h3 class="panel-title">post title</h3>
+  </div>
+  <div class="panel-body">
+    <p>body</p>
+
+
+ <img src="" class="img-thumbnail"  style="width:50%;height:50%" >
+
+   <span class="label label-danger">created at :12:00  </span>
+     <span class="label label-info">  by :  ######</span>
+
+      <a href="#" class="float-right">View Post</a>
+
+  </div>
+</div>
+
+  </div>
+
+</div>
+
+</div>
+
+</div>
+
+           @endsection
---
+++ resources/views/posts/viewPost.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row container">
+
+            <div class="col-sm-8" style="margin: auto;">
+
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">title</h3>
+                    </div>
+                    <div class="panel-body">
+
+                       <div class="text-center">
+                        <p class="w-75 text-left m-auto"> body</p>
+
+
+                            <img src="https://loremflickr.com/320/240" class="img-thumbnail m-auto d-block w-75 mh-60"  style="width:50%;height:50%" >
+
+
+                            <video width="320" height="240" controls style="display: block;" class="m-auto d-block w-75">
+                                <source src="http://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4">
+                                <source src="http://www.w3schools.com/html/mov_bbb.mp4" type="video/ogg">
+                                Your browser does not support the video tag.
+                            </video>
+
+                            <a href="#" class="m-auto d-block w-75">post file</a>
+                       </div>
+
+<div class="text-center" style="  display:block">
+                        <span class="label label-danger">created at : 12:00  </span>
+                             <span class="label label-info">  by name</span>
+
+
+                                  <div style="  display:inline-block">
+                                    <a href="#"><i class="fa fa-edit"></i></a>
+                                    <a href="#"onclick="if(!confirm('Do you Delete This Post ?')) return false"><i class="fa fa-trash"></i></a>
+                                </div>
+                            </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+    </div>
+
+@endsection
