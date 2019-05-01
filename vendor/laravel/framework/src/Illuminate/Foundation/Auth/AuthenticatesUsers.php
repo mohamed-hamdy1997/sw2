f652f03367f383ee98e21a21355131569b318f0b
tTeha <tteha43@gmail.com>
2019-05-01 03:21:39 +0200

    Update AuthenticatesUsers.php

--- vendor/laravel/framework/src/Illuminate/Foundation/Auth/AuthenticatesUsers.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Auth/AuthenticatesUsers.php
@@ -28,8 +28,32 @@ trait AuthenticatesUsers
      *
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function login(Request $request)
-    {
+
+//    public function login(Request $request)
+//    {
+//        $this->validateLogin($request);
+//
+//        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+//        // the login attempts for this application. We'll key this by the username and
+//        // the IP address of the client making these requests into this application.
+//        if ($this->hasTooManyLoginAttempts($request)) {
+//            $this->fireLockoutEvent($request);
+//
+//            return $this->sendLockoutResponse($request);
+//        }
+//
+//        if ($this->attemptLogin($request)) {
+//            return $this->sendLoginResponse($request);
+//        }
+//
+//        // If the login attempt was unsuccessful we will increment the number of attempts
+//        // to login and redirect the user back to the login form. Of course, when this
+//        // user surpasses their maximum number of attempts they will get locked out.
+//        $this->incrementLoginAttempts($request);
+//
+//        return $this->sendFailedLoginResponse($request);
+//    }
+    public function login(\Illuminate\Http\Request $request) {
         $this->validateLogin($request);
 
         // If the class is using the ThrottlesLogins trait, we can automatically throttle
@@ -37,12 +61,26 @@ trait AuthenticatesUsers
         // the IP address of the client making these requests into this application.
         if ($this->hasTooManyLoginAttempts($request)) {
             $this->fireLockoutEvent($request);
-
             return $this->sendLockoutResponse($request);
         }
 
-        if ($this->attemptLogin($request)) {
-            return $this->sendLoginResponse($request);
+        // This section is the only change
+        if ($this->guard()->validate($this->credentials($request))) {
+            $user = $this->guard()->getLastAttempted();
+
+            // Make sure the user is active
+            if ($user->active && $this->attemptLogin($request)) {
+                // Send the normal successful login response
+                return $this->sendLoginResponse($request);
+            } else {
+                // Increment the failed login attempts and redirect back to the
+                // login form with an error message.
+                $this->incrementLoginAttempts($request);
+                return redirect()
+                    ->back()
+                    ->withInput($request->only($this->username(), 'remember'))
+                    ->withErrors(['active' => 'You must be active to login.']);
+            }
         }
 
         // If the login attempt was unsuccessful we will increment the number of attempts
@@ -104,7 +142,7 @@ trait AuthenticatesUsers
         $this->clearLoginAttempts($request);
 
         return $this->authenticated($request, $this->guard()->user())
-                ?: redirect()->intended($this->redirectPath());
+            ?: redirect()->intended($this->redirectPath());
     }
 
     /**
